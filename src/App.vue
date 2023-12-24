63316f44b55ab90c3117c0878c0849e6c0cd00e0
Mauricio Sousa <mauriciosouza8989@gmail.com>
2023-12-24 12:57:05 -0300

    finalizando exercício

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { reactive } from 'vue';
+import Formulario from './components/Formulario.vue';
+
+    const estado = reactive({
+        option: 'adicao',
+        firstOperator: 0,
+        secondOperator: 0,
+        operator: '+',
+    })
+    function calculo(){
+        const {option, firstOperator, secondOperator} = estado;
+        switch(option){
+            case 'adicao': return changeOperatorAndCalc()
+            case 'subtracao': return changeOperatorAndCalc()
+            case 'multiplicacao': return changeOperatorAndCalc()
+            case 'divisao':return changeOperatorAndCalc()
+        }
+    }
+    function changeOperatorAndCalc(){
+        const {option, firstOperator, secondOperator} = estado;
+        if(option == 'adicao'){
+            estado.operator = '+' 
+            return Number(firstOperator) + Number(secondOperator);
+        }
+        if(option == 'subtracao'){ 
+            estado.operator = '-';
+            return  Number(firstOperator) - Number(secondOperator);
+        }
+        if(option == 'multiplicacao'){ 
+            estado.operator = '*' 
+            return  Number(firstOperator) * Number(secondOperator);
+        }
+        if(option == 'divisao'){ 
+            estado.operator = '/' 
+            if(estado.secondOperator <= 0 ){
+                estado.secondOperator = 1
+            }
+            return  Number(firstOperator) / Number(secondOperator);
+        }
+    }
+    function getFirstValue(event){
+        return estado.firstOperator = event.target.value;
+    }
+    function getSecondValue(event){
+        return estado.secondOperator = event.target.value;
+    }
+
+</script>
+
+<template>
+    <div class="container">
+        <Formulario
+            :estado="estado" 
+            :get-first-value="getFirstValue" 
+            :get-second-value="getSecondValue"
+            :calculo="calculo"
+        />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,29 @@
+<script setup>
+    const props = defineProps(['estado', 'getFirstValue', 'getSecondValue', 'calculo'])
+</script>
+<template>
+      <div class=" mt-5 d-flex flex-column align-items-center">
+            <h1>Faça um calculo</h1>
+        <form>
+            <div class="row mt-3">
+                <div class="col">
+                    <label for="calculo-options">Escolha a opção do calculo</label>
+                    <select @change="event=>props.estado.option=event.target.value" id="calculo-options" class="form-control">
+                        <option value="adicao">Adição</option>
+                        <option value="subtracao">Subtração</option>
+                        <option value="multiplicacao">Multiplicação</option>
+                        <option value="divisao">Divisão</option>
+                    </select>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <h3 class="mt-5 mb-3">Resultado: <span>{{ props.calculo() }}</span></h3>
+                <div class="d-flex gap-1">
+                    <input @keyup="props.getFirstValue" class="form-control" type="number" placeholder="Primeiro operador">
+                    <span class="h3">{{ props.estado.operator }}</span>
+                    <input @keyup="props.getSecondValue" class="form-control" type="number" placeholder="Segundo operador">
+                </div>
+            </div>
+        </form>
+        </div>
+</template>
